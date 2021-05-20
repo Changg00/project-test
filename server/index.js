8609000a2ab823c75da639b3efa24ae2f2b953ca
unknown <jmires00@gmail.com>
2021-05-20 14:38:15 -0600

    server change

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const massive = require('massive');
 
 const { format } = require('date-fns');
 
-const path = require('path')
+// const path = require('path')
 
 const {CONNECTION_STRING, SERVER_PORT, SESSION_SECRET} = process.env
 
@@ -36,7 +36,7 @@ app.use(
 // Authorization Endpoints
 app.post('/api/auth/register', ctrlUser.register)
 app.post('/api/auth/login', ctrlUser.login)
-app.get('/api/auth/user', ctrlUser.getUser)
+app.get('/api/auth/user', ctrlUser.getUser)``
 app.post('/api/auth/logout', ctrlUser.logout)
 app.put('/api/auth/update/:id', ctrlUser.editUser)
 
@@ -51,9 +51,9 @@ app.delete('/api/expense/:id', auth.userOnly, ctrlExpense.deleteExpense)
 // HOSTING//
 app.use(express.static(__dirname + '/../build'))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../build/index.html'))
+// })
 
 app.listen(SERVER_PORT, _ => {
   console.log(`Hi! I'm your server and I'm listening on port: ${SERVER_PORT}! This is the best thing EVER!`)
