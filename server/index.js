d691b578b46080a57102836ef3230347e9e55065
unknown <jmires00@gmail.com>
2021-05-20 14:43:06 -0600

    server 2

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const massive = require('massive');
 
 const { format } = require('date-fns');
 
-// const path = require('path')
+const path = require('path')
 
 const {CONNECTION_STRING, SERVER_PORT, SESSION_SECRET} = process.env
 
@@ -33,15 +33,15 @@ app.use(
   })
 )
 
-// Authorization Endpoints
+// // Authorization Endpoints
 app.post('/api/auth/register', ctrlUser.register)
 app.post('/api/auth/login', ctrlUser.login)
-app.get('/api/auth/user', ctrlUser.getUser)``
+app.get('/api/auth/user', ctrlUser.getUser)
 app.post('/api/auth/logout', ctrlUser.logout)
 app.put('/api/auth/update/:id', ctrlUser.editUser)
 
-// Expense Endpoints
-// app.post('/api/read-day', auth.userOnly, ctrlExpense.readDayExpense)
+// // Expense Endpoints
+app.post('/api/read-day', auth.userOnly, ctrlExpense.readDayExpense)
 app.post('/api/new-expense', auth.userOnly, ctrlExpense.createExpense)
 app.post('/api/expenses-range', auth.userOnly, ctrlExpense.readRangeExpenses)
 app.put('/api/edit-expense/:id', auth.userOnly, ctrlExpense.editExpense)
@@ -51,9 +51,9 @@ app.delete('/api/expense/:id', auth.userOnly, ctrlExpense.deleteExpense)
 // HOSTING//
 app.use(express.static(__dirname + '/../build'))
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../build/index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
 
 app.listen(SERVER_PORT, _ => {
   console.log(`Hi! I'm your server and I'm listening on port: ${SERVER_PORT}! This is the best thing EVER!`)
